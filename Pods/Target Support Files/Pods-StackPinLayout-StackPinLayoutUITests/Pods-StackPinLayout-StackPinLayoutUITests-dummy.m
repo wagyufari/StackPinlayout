3561702d6beb4f57546cbc48a700f1705d82f486
Muhammad Ghifari <ghifari9567@gmail.com>
2021-08-18 21:10:53 +0700

    Initial

---
+++ Pods/Target Support Files/Pods-StackPinLayout-StackPinLayoutUITests/Pods-StackPinLayout-StackPinLayoutUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_StackPinLayout_StackPinLayoutUITests : NSObject
+@end
+@implementation PodsDummy_Pods_StackPinLayout_StackPinLayoutUITests
+@end
--- StackPinLayout/ViewController.swift
+++ StackPinLayout/ViewController.swift
@@ -1,19 +1,92 @@
 //
 //  ViewController.swift
-//  StackPinLayout
+//  MentionTextView
 //
-//  Created by Muhammad Ghifari on 18/08/21.
+//  Created by Muhammad Ghifari on 29/07/21.
 //
 
 import UIKit
+import PinLayout
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController{
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view = ViewControllerView()
+        view.backgroundColor = .white
     }
-
-
 }
 
+class ViewControllerView:UIView{
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    let stack:UIStackPinView={
+        return UIStackPinView()
+    }()
+    
+    var scroll:UIScrollView = {
+       return UIScrollView()
+    }()
+    
+    init() {
+        super.init(frame: .zero)
+        addSubview(scroll)
+        scroll.addSubview(stack)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        performLayout()
+        didPerformLayout()
+    }
+    
+    func performLayout() {
+        scroll.pin.all()
+        for i in 0...5 {
+            stack.addArrangedSubview(UIView().apply{
+                $0.pin.height(200)
+                $0.pin.width(200)
+                $0.backgroundColor = .blue
+            })
+            stack.addArrangedSubview(UIView().apply{
+                $0.pin.height(200)
+                $0.pin.width(200)
+                $0.backgroundColor = .green
+            })
+        }
+    }
+    
+    func didPerformLayout() {
+        scroll.contentSize = CGSize(width: scroll.bounds.width, height: stack.frame.maxY)
+    }
+    override func sizeThatFits(_ size: CGSize) -> CGSize {
+        autoSizeThatFits(size, layoutClosure: performLayout)
+    }
+}
+
+class UIStackPinView: UIView{
+    
+    var subViews:[View] = []
+    
+    func addArrangedSubview(_ view:View)  {
+        subViews.append(view)
+        setNeedsLayout()
+        layoutIfNeeded()
+        pin.all().height(subviews.map{$0.frame.height}.reduce(0, +))
+    }
+    
+    override func layoutSubviews() {
+        subViews.forEach { UIView in
+            addSubview(UIView)
+        }
+        if subViews.count > 1 {
+            for (index, view) in subViews.enumerated() {
+                if index != 0 {
+                    view.pin.below(of: subViews[index-1])
+                }
+            }
+        }
+    }
+}
